28573032be2464ff2f5fdba985ee8892647572a2
Joey <wyjs97115@sina.com>
2018-10-19 22:17:25 +0800

    update map styles

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="container">
-      <!-- <v-touch @panmove="onMove" @click="onTap" tag="svg" ref="s" :style="{transform: trans}"></v-touch> -->
-    <svg ref="svg" @click="onRotate"></svg>
-  </div>
+  <v-touch @panmove="onMove" @tap="onTap" class="container">
+    <svg></svg>
+  </v-touch>
+  <!-- <div class="container">
+    <v-touch @panmove="onMove" @click="onTap" tag="svg" :style="{transform: trans}"></v-touch>
+  </div> -->
 </template>
 
 <script>
 import * as d3 from 'd3';
 export default {
   name: 'Map',
-  props: ['data', 'transform'],
+  props: ['data', 'rotate'],
   data () {
     return {
       COLOR_MAP: {
@@ -23,16 +25,18 @@ export default {
     }
   },
   watch: {
-    transform: function (oldVal, newVal) {
-
-      this.trans = `rotate(${newVal}deg)`;
+    rotate: function (newVal, oldVal) {
+      if (newVal === 0) console.log(oldVal, newVal)
       d3.select('svg').transition()
-        .style('transform', `rotate(${newVal - 90}deg)`)
+        .style('transform', `rotate(${newVal}deg)`)
       d3.selectAll('text').transition()
-        // .attr('x', (d, i) => d.x + d.w / 2 + 15 * Math.sin(newVal - 90))
-        // .attr('y', (d, i) => d.y + d.h / 2 + 15 * Math.cos(newVal - 90))
-        .attr('transform', (d, i) => `rotate(${90 - newVal},${d.x + d.w / 2},${d.y + d.h / 2})`)
-        // .attr('x', (d,i) => d)
+        .attr('x', (d, i) => d.x + d.w / 2 + d.d * Math.sin((newVal) * Math.PI / 180))
+        .attr('y', (d, i) => d.y + d.h / 2 + d.d * Math.cos((newVal) * Math.PI / 180))
+        .attr('transform', (d, i) => `rotate(${-newVal},${d.x + d.w / 2},${d.y + d.h / 2})`)
+    },
+    data: function (newVal, oldVal) {
+      console.log('prop updated', newVal)
+      this.renderMap()
     }
   },
   methods: {
@@ -51,104 +55,104 @@ export default {
       return [x, y]
     },
     onMove (e) {
+      console.log(e)
       d3.select('svg').transition()
         .attr('transform', `translate(${e.deltaX},${e.deltaY})`)
     },
     onRotate (d) {
       d3.select('svg').transition()
         .style('transform', `rotate(${this.transform}deg)`)
-      console.log(this.trans)
     },
     onTap (e) {
-      // this.$refs.s.tranform = this.trans
-      // d3.select('svg').transition()
-      //   .attr('transform', `translate(${e.center.x},${e.center.y})`)
-      // d3.select('svg').transition()
-      //   .style('transform', `translate(${e.screenX}px,${e.screenY}px)`)
-      console.log(e)
-    }
-  },
-  mounted () {
-    const mapSize = this.calcSize()
-    const dataset = this.data.blocks
-    const map = d3.select('svg')
-      .attr('class', 'map')
-      .attr('preserveAspectRatio', 'xMidYMid meet')
-      .attr('viewBox', `0 0 ${mapSize[0]} ${mapSize[1]}`)
-    const blocks = map.append('g')
-      .selectAll('g')
-      .data(dataset)
-      .enter()
-      .append('g')
-      .on('click', function () {
-        let curColor = d3.select(this).select('rect').attr('stroke')
-        d3.select(this).select('rect')
-          // .transition()
-          // .duration(300)
-          .attr('stroke', curColor === '#fff' ? '#ff534d' : '#fff')
-      })
+      this.$emit('svg-tapped')
+    },
+    renderMap () {
+      const mapSize = this.calcSize()
+      const dataset = this.data.blocks
+      const map = d3.select('svg')
+        .attr('class', 'map')
+        .attr('preserveAspectRatio', 'xMidYMid meet')
+        .attr('viewBox', `0 0 ${mapSize[0]} ${mapSize[1]}`)
+      const blocks = map.append('g')
+        .selectAll('g')
+        .data(dataset)
+        .enter()
+        .append('g')
+        .on('click', function () {
+          let curColor = d3.select(this).select('rect').attr('stroke')
+          d3.select(this).select('rect')
+            // .transition()
+            // .duration(300)
+            .attr('stroke', curColor === '#fff' ? '#ff534d' : '#fff')
+        })
 
-    blocks.append('rect')
-      .attr('x', (d, i) => d.data.x + 2)
-      .attr('y', (d, i) => d.data.y + 2)
-      .attr('width', (d, i) => d.data.width - 4)
-      .attr('height', (d, i) => d.data.height - 4)
-      .attr('fill', (d, i) => this.COLOR_MAP[d.type])
-      .attr('stroke', '#fff')
-      .attr('stroke-width', 4)
-      .attr('stroke-linejoin', 'bevel')
+      blocks.append('rect')
+        .attr('x', (d, i) => d.data.x + 2)
+        .attr('y', (d, i) => d.data.y + 2)
+        .attr('width', (d, i) => d.data.width - 4)
+        .attr('height', (d, i) => d.data.height - 4)
+        .attr('fill', (d, i) => this.COLOR_MAP[d.type])
+        .attr('stroke', '#fff')
+        .attr('stroke-width', 4)
+        .attr('stroke-linejoin', 'bevel')
 
-    blocks.selectAll('text')
-      .data((d) => {
-        if (d.name.length || d.numbers.length === 1) {
-          return ([{
-            name: d.name || d.numbers[0],
-            x: d.data.x,
-            y: d.data.y,
-            w: d.data.width,
-            h: d.data.height
-          }])
-        } else {
-          return d.numbers.map((item, index) => {
-            return ({
-              name: item,
+      blocks.selectAll('text')
+        .data((d) => {
+          if (d.name.length || d.numbers.length === 1) {
+            return ([{
+              name: d.name || d.numbers[0],
               x: d.data.x,
               y: d.data.y,
               w: d.data.width,
-              h: index === 0 ? d.data.height - 30 : d.data.height + 30
+              h: d.data.height,
+              d: 0
+            }])
+          } else {
+            return d.numbers.map((item, index) => {
+              return ({
+                name: item,
+                x: d.data.x,
+                y: d.data.y,
+                w: d.data.width,
+                h: d.data.height,
+                d: index === 0 ? 18 : -18
+              })
             })
-          })
-        }
-      })
-      .enter()
-      .append('text')
-      .text((d) => d.name)
-      .attr('x', (d, i) => d.x + d.w / 2)
-      .attr('y', (d, i) => d.y + d.h / 2)
-      .attr('fill', '#595959')
-      .attr('font-size', '2rem')
-      .attr('font-family', '"Consolas","Courier New",Monaco,sans-serif')
-      .attr('text-anchor', 'middle')
-      .attr('dominant-baseline', 'middle')
-      .attr('font-weight', 'bold')
+          }
+        })
+        .enter()
+        .append('text')
+        .text((d) => d.name)
+        .attr('x', (d, i) => d.x + d.w / 2 + d.d * Math.sin(0))
+        .attr('y', (d, i) => d.y + d.h / 2 + d.d * Math.cos(0))
+        .attr('fill', '#595959')
+        .attr('font-size', '2rem')
+        .attr('font-family', '"Consolas","Courier New",Monaco,sans-serif')
+        .attr('text-anchor', 'middle')
+        .attr('dominant-baseline', 'middle')
+        .attr('font-weight', 'bold')
 
-    // const text = blocks.selectAll('text')
-    //   .data((d) => d)
-    //   .enter()
-    //   .append('text')
-    //   .text((d, i) => {
-    //     if (d.type === 1) return d.numbers[0]
-    //     if (d.type === 2) return '🚶'
-    //     if (d.type === 3) return '🚰'
-    //     if (d.type === 4) return '🚻'
-    //     if (d.type === 5) return '☕'
-    //   })
-    //   .attr('x', (d, i) => d.data.x + d.data.width / 2)
-    //   .attr('y', (d, i) => d.data.y + d.data.height / 2)
-    //   .attr('font-size', '2rem')
-    //   .attr('font-family', '"Consolas","Courier New",Monaco,sans-serif')
-    //   .attr('text-anchor', 'middle')
-    //   .attr('dominant-baseline', 'middle')
+      // const text = blocks.selectAll('text')
+      //   .data((d) => d)
+      //   .enter()
+      //   .append('text')
+      //   .text((d, i) => {
+      //     if (d.type === 1) return d.numbers[0]
+      //     if (d.type === 2) return '🚶'
+      //     if (d.type === 3) return '🚰'
+      //     if (d.type === 4) return '🚻'
+      //     if (d.type === 5) return '☕'
+      //   })
+      //   .attr('x', (d, i) => d.data.x + d.data.width / 2)
+      //   .attr('y', (d, i) => d.data.y + d.data.height / 2)
+      //   .attr('font-size', '2rem')
+      //   .attr('font-family', '"Consolas","Courier New",Monaco,sans-serif')
+      //   .attr('text-anchor', 'middle')
+      //   .attr('dominant-baseline', 'middle')
+    }
+  },
+  mounted () {
+    this.renderMap()
   }
 }
 </script>
--- src/views/Floor.vue
+++ src/views/Floor.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="container">
-    <p class="float nav"
-      v-show="showCtrls">
-      {{ campus }} {{ building }} {{ floor }} {{ room }}
-    </p>
-    <p class="float ctrls"
-      v-show="showCtrls"
-      v-if="orientation >= 0">
-      {{ orientation }}
-    </p>
+    <transition name="fade">
+      <div class="float">
+        <p class="nav"
+          v-show="showCtrls">
+          {{ campus }} {{ building }} {{ floor }} {{ room }}
+        </p>
+        <div class="orientation"
+          v-show="showCtrls"
+          @click="switchRotation">
+          <img src="../assets/images/compass.png" :style="{transform: `rotate(${rotation - 45}deg)`}">
+          <p>跟随方向:{{ enableRotation ? 'ON' : 'OFF' }}</p>
+        </div>
+      </div>
+    </transition>
     <floor-map class="map"
       v-if="buildingData.blocks"
       :data="buildingData"
-      :transform="orientation"
+      :rotate="rotation"
+      @svg-tapped="showCtrls = !showCtrls"
     />
   </div>
 </template>
@@ -33,9 +39,19 @@ export default {
       floor: this.$route.query.floor || 0,
       room: this.$route.query.room || -1,
       buildingData: {},
-      orientation: 0,
+      enableRotation: true,
+      shouldUpdateRotation: true,
+      rotation: 0,
       showCtrls: true
     };
+  },
+  watch: {
+    $route: {
+      handler: function (val) {
+        this.onRoute(val)
+      },
+      deep: true
+    }
   },
   methods: {
     getMapData () {
@@ -46,28 +62,74 @@ export default {
           floor: this.floor
         })
         .then(data => {
-          console.log(data);
-          this.buildingData = data;
+          console.log('floor data:', data)
+          this.buildingData = data
         });
     },
     getOrientation () {
       if (window.DeviceOrientationEvent) {
-        window.addEventListener('deviceorientation', (event) => {
-          if (Math.abs(event.alpha - this.orientation) < 2) return
-          this.orientation = Math.floor(event.alpha)
-        })
+        window.addEventListener('deviceorientation', this.updateRotation)
+      } else {
+        alert('不支持罗盘')
+      }
+    },
+    updateRotation (event) {
+      let alpha = 0
+      if (event.webkitCompassHeading) {
+        alpha = event.webkitCompassHeading
+      } else if (event.absolute) {
+        alpha = event.alpha
+      }
+      if (Math.abs(alpha - this.rotation) < 2) return
+      if (!this.shouldUpdateRotation) return
+      this.rotation = Math.floor(360 - alpha)
+    },
+    switchRotation () {
+      if (this.enableRotation) {
+        window.removeEventListener('deviceorientation', this.updateRotation)
+        this.enableRotation = false
+        this.rotation = 0
+      } else {
+        this.enableRotation = true
+        this.getOrientation()
+      }
+    },
+    getMotion () {
+      if (window.DeviceMotionEvent) {
+        window.addEventListener('devicemotion', this.watchMotion)
+      }
+    },
+    watchMotion (event) {
+      this.shouldUpdateRotation = !event.acceleration.x > 0.01
+    },
+    onRoute (val) {
+      if (val.query.campus !== this.campus) {
+        console.log('campus changed')
+        this.campus = val.query.campus
+        this.getMapData()
+      } else if (val.query.building !== this.building) {
+        console.log('building changed')
+        this.building = val.query.building
+        this.getMapData()
+      } else if (val.query.floor !== this.floor) {
+        console.log('floor changed')
+        this.floor = val.query.floor
+        this.getMapData()
+      } else if (val.query.index !== this.room) {
+        console.log('index changed', val.query.index)
       }
     }
   },
   mounted () {
     this.getMapData()
     this.getOrientation()
+    this.getMotion()
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
 html, body {
   height: 100%;
   width: 100%;
@@ -82,14 +144,41 @@ html, body {
 .nav {
   position: absolute;
 }
-.ctrls {
-  position: absolute;
-}
 .map {
   position: absolute;
   box-sizing: border-box;
   z-index: 0;
   width: 100%;
   height: 100%;
+}
+
+.orientation {
+  position: absolute;
+  bottom: 2rem;
+  left: 2rem;
+  background-color: #fff;
+  box-shadow: 0 0 0.05em #000;
+  padding: .4rem;
+  border-radius: .4rem;
+  -webkit-appearance: none;
+  -webkit-box-shadow: 0 0 .3rem #ccc;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+.orientation img {
+  width: 2rem;
+  height: 2rem;
+}
+p {
+  margin: 0;
+  z-index: 2;
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .3s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
 }
 </style>
